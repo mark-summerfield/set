1b8fcadbc3169f9e3871ed08719fbfd010dcfab0
mark-summerfield <mark@qtrac.eu>
2025-04-09 10:03:27 +0100

    m

--- set.go
+++ set.go
@@ -1,4 +1,4 @@
-// Copyright © 2024 Mark Summerfield. All rights reserved.
+// Copyright © 2024-25 Mark Summerfield. All rights reserved.
 
 // ([TOC]) This package provides a generic unordered set implementation
 // (using a map under the hood).
@@ -15,7 +15,7 @@ import (
 
 type Set[E comparable] struct{ set map[E]struct{} }
 
-// New returns a new set containing the given elements (if any).
+// New returns a new Set containing the given elements (if any).
 // If no elements are given, the type must be specified since it can't be
 // inferred.
 func New[E comparable](elements ...E) Set[E] {
@@ -26,39 +26,38 @@ func New[E comparable](elements ...E) Set[E] {
 	return set
 }
 
-// Add adds the given element(s) to the set.
+// Add adds the given element(s) to the Set.
 func (me *Set[E]) Add(elements ...E) {
 	for _, element := range elements {
 		me.set[element] = struct{}{}
 	}
 }
 
-// Delete deletes the given element(s) from the set.
+// Delete deletes the given element(s) from the Set.
 func (me *Set[E]) Delete(elements ...E) {
 	for _, element := range elements {
 		delete(me.set, element)
 	}
 }
 
-// Clear deletes all the elements to make this an empty set.
+// Clear deletes all the elements in the Set.
 func (me *Set[E]) Clear() { clear(me.set) }
 
-// Len returns the number of elements in the set.
+// Len returns the number of elements in the Set.
 func (me *Set[E]) Len() int { return len(me.set) }
 
-// IsEmpty returns true if there are no elements in the set; otherwise
+// IsEmpty returns true if there are no elements in the Set; otherwise
 // returns false.
 func (me *Set[E]) IsEmpty() bool { return len(me.set) == 0 }
 
-// Contains returns true if element is in the set; otherwise returns false.
-// Alternatively, use map syntax.
+// Contains returns true if element is in the Set; otherwise returns false.
 func (me *Set[E]) Contains(element E) bool {
 	_, ok := me.set[element]
 	return ok
 }
 
-// Difference returns a new set that contains the elements which are in this
-// set that are not in the other set.
+// Difference returns a new Set that contains the elements which are in this
+// Set that are not in the other Set.
 func (me *Set[E]) Difference(other Set[E]) Set[E] {
 	diff := New[E]()
 	for element := range me.set {
@@ -69,8 +68,8 @@ func (me *Set[E]) Difference(other Set[E]) Set[E] {
 	return diff
 }
 
-// SymmetricDifference returns a new set that contains the elements which
-// are in this set or the other set—but not in both sets.
+// SymmetricDifference returns a new Set that contains the elements which
+// are in this Set or the other Set—but not in both Sets.
 func (me *Set[E]) SymmetricDifference(other Set[E]) Set[E] {
 	diff := New[E]()
 	for element := range me.set {
@@ -86,8 +85,8 @@ func (me *Set[E]) SymmetricDifference(other Set[E]) Set[E] {
 	return diff
 }
 
-// Intersection returns a new set that contains the elements this set has in
-// common with the other set.
+// Intersection returns a new Set that contains the elements this Set has in
+// common with the other Set.
 func (me *Set[E]) Intersection(other Set[E]) Set[E] {
 	intersection := New[E]()
 	for element := range me.set {
@@ -98,8 +97,8 @@ func (me *Set[E]) Intersection(other Set[E]) Set[E] {
 	return intersection
 }
 
-// Union returns a new set that contains the elements from this set and from
-// the other set (with no duplicates of course).
+// Union returns a new Set that contains the elements from this Set and from
+// the other Set (with no duplicates of course).
 // See also [Set.Unite].
 func (me *Set[E]) Union(other Set[E]) Set[E] {
 	union := me.Clone()
@@ -107,8 +106,8 @@ func (me *Set[E]) Union(other Set[E]) Set[E] {
 	return union
 }
 
-// Unite adds all the elements from other that aren't already in this set to
-// this set.
+// Unite adds all the elements from other that aren't already in this Set to
+// this Set.
 // See also [Set.Union].
 func (me *Set[E]) Unite(other Set[E]) {
 	for element := range other.set {
@@ -116,19 +115,19 @@ func (me *Set[E]) Unite(other Set[E]) {
 	}
 }
 
-// Clone returns a copy of this set.
+// Clone returns a copy of this Set.
 func (me *Set[E]) Clone() Set[E] {
 	return Set[E]{maps.Clone(me.set)}
 }
 
-// Equal returns true if this set has the same elements as the other set;
+// Equal returns true if this Set has the same elements as the other Set;
 // otherwise returns false.
 func (me *Set[E]) Equal(other Set[E]) bool {
 	return maps.Equal(me.set, other.set)
 }
 
-// IsDisjoint returns true if this set has no elements in common with the
-// other set; otherwise returns false.
+// IsDisjoint returns true if this Set has no elements in common with the
+// other Set; otherwise returns false.
 func (me *Set[E]) IsDisjoint(other Set[E]) bool {
 	for element := range me.set {
 		if _, ok := other.set[element]; ok {
@@ -138,8 +137,8 @@ func (me *Set[E]) IsDisjoint(other Set[E]) bool {
 	return true
 }
 
-// IsSubsetOf returns true if this set is a subset of the other set, i.e.,
-// if every member of this set is in the other set; otherwise returns false.
+// IsSubsetOf returns true if this Set is a subset of the other Set, i.e.,
+// if every member of this Set is in the other Set; otherwise returns false.
 func (me *Set[E]) IsSubsetOf(other Set[E]) bool {
 	for element := range me.set {
 		if _, ok := other.set[element]; !ok {
@@ -149,14 +148,14 @@ func (me *Set[E]) IsSubsetOf(other Set[E]) bool {
 	return true
 }
 
-// IsSupersetOf returns true if this set is a superset of the other set,
-// i.e., if every member of the other set is in this set; otherwise returns
+// IsSupersetOf returns true if this Set is a superset of the other Set,
+// i.e., if every member of the other Set is in this Set; otherwise returns
 // false.
 func (me Set[E]) IsSupersetOf(other Set[E]) bool {
 	return other.IsSubsetOf(me)
 }
 
-// All returns an iterator, e.g., for element := range set.All() ...
+// All returns an iterator, e.g., for element := range aset.All() ...
 func (me *Set[E]) All() iter.Seq[E] {
 	return func(yield func(E) bool) {
 		for key := range me.set {
@@ -168,7 +167,7 @@ func (me *Set[E]) All() iter.Seq[E] {
 }
 
 // AllX returns an iterator, e.g.,
-// for count, element := range set.AllX(1) ...
+// for count, element := range aset.AllX(1) ...
 func (me *Set[E]) AllX(start ...int) iter.Seq2[int, E] {
 	return func(yield func(int, E) bool) {
 		i := 0
@@ -184,7 +183,7 @@ func (me *Set[E]) AllX(start ...int) iter.Seq2[int, E] {
 	}
 }
 
-// ToSlice returns this set's elements as an unsorted slice.
+// ToSlice returns this Set's elements as an unsorted slice.
 // For iteration either use this, or if you only need one value at a time,
 // use [All] or [AllX]. To sort, use slices.Sorted (if E is cmp.Orderable).
 func (me *Set[E]) ToSlice() []E {
@@ -195,7 +194,7 @@ func (me *Set[E]) ToSlice() []E {
 	return slice
 }
 
-// String returns a human readable string representation of the set.
+// String returns a human readable string representation of the Set.
 func (me *Set[E]) String() string {
 	format := "%s%v"
 	if me.hasStringElements() {
